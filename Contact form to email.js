3701f2c315b2234ac764ea6a2b5d0787c7f28880
Coachflux <110670400+Coachflux@users.noreply.github.com>
2024-11-16 08:43:30 +0100

    Update Contact form to email.js

--- Contact form to email.js
+++ Contact form to email.js
@@ -1,17 +1,17 @@
 function emailSend(){
 
 	var userName = document.getElementById('name').value;
-	var phone = document.getElementById('phone').value;
+	var subject = document.getElementById('subject').value;
 	var email = document.getElementById('email').value;
 
 	var messageBody = "Name " + userName +
-	"<br/> Phone " + phone +
+	"<br/> subject " + subject +
 	"<br/> Email " + email;
 	Email.send({
     Host : "smtp.elasticemail.com",
     Username : "host@gmail.com",
     Password : "Password",
-    To : 'reviever@gmail.com',
+    To : 'icenetworklab@gmail.com',
     From : "website@gmail.com",
     Subject : "This is the subject",
     Body : messageBody
